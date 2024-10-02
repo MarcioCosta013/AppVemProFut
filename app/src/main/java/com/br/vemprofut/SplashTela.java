84f7ef8879cf762e329d34684425f95f9d49ac3b
Marcio Costa <marcio-costa38@hotmail.com>
2024-10-02 15:33:19 -0300

    Adicionando imagens e a activity de Splash

---
+++ app/src/main/java/com/br/vemprofut/SplashTela.java
@@ -0,0 +1,40 @@
+package com.br.vemprofut;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splash_tela);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ActivitySenha();
+            }
+        },3000);
+    }
+
+    private void ActivitySenha (){
+        Intent intent = new Intent(SplashTela.this, MainActivity.class); //indica a intenção de navegar entre activity's.
+        startActivity(intent); //inicia a activity.
+        finish(); //finaliza a activity.
+
+    }
+}
